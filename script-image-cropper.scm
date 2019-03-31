0060d632000943905d82f1d838b95fb0ddb52136
Naoro <naoro.artist@gmail.com>
2019-04-01 00:45:01 +0200

    Fixed error when specifying negative dimensions (which means to not crop)

--- script-image-cropper.scm
+++ script-image-cropper.scm
@@ -6,31 +6,23 @@
      (drawable (car (gimp-image-get-active-drawable croppedImage)))
      (origWidth (car (gimp-image-width srcImage)))
      (origHeight (car (gimp-image-height srcImage)))
-     (offsetX
+     (croppedWidth
        (if (< width 0)
-         0 ; If given (crop) width is less than 0, use full width (don't crop)
-         (if (> width origWidth)
-           origWidth ; If givien (crop) width is greater than image width
-                     ; Crop all the image width
-                     ; Should be replaced to give an error and exit
-           (/ (- origWidth width) 2) ; Offset is set so image is horizontally centered
-         )
+         origWidth ; If given (crop) width is less than 0, use full width (don't crop)
+         width
        )
      )
-     (offsetY
+     (croppedHeight
        (if (< height 0)
-         0 ; If given (crop) height is less than 0, use full height (don't crop)
-         (if (> width origHeight)
-           origHeight ; If givien (crop) height is greater than image height
-                     ; Crop all the image height
-                     ; Should be replaced to give an error and exit
-           (/ (- origHeight height) 2) ; Offset is set so image is vertically centered
-         )
+         origHeight ; If given (crop) height is less than 0, use full height (don't crop)
+         height
        )
      )
+     (offsetX (/ (- origWidth croppedWidth) 2)) ; Offset is set so image is horizontally centered
+     (offsetY (/ (- origHeight croppedHeight) 2)) ; Offset is set so image is vertically centered  
     )
     ; Crops the image
-    (gimp-image-crop croppedImage width height offsetX offsetY)
+    (gimp-image-crop croppedImage croppedWidth croppedHeight offsetX offsetY)
     
     ; Used to debug, removed to use it in non interactive mode
     ;(gimp-display-new srcImage) 
